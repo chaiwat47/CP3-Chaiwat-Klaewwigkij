15a6d7827e3dad26e74294c50bc1972d2dc4fa1a
chaiwat47 <58399208+chaiwat47@users.noreply.github.com>
2020-01-17 08:53:58 +0700

    Add files via upload

---
+++ Exercise4_Chaiwat_K.py
@@ -0,0 +1,12 @@
+#Input_variables
+FE_point=float(input("Foundation English:"))
+GB_point=float(input("General Business:"))
+ICS_point=float(input("Introduction to Computer Systems:"))
+CP_point=float(input("Computer Programming:"))
+
+#Output
+print("---","Your Score","---")
+print("Foundation English:",FE_point)
+print("General Business:",GB_point)
+print("Introduction to Computer Systems:",ICS_point)
+print("Computer Programming:",CP_point)
