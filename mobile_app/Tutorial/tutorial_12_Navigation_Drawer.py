70cadf9dd77fc5c70c820da0f5f93f642dd15144
chaiwat47 <58399208+chaiwat47@users.noreply.github.com>
2020-11-05 02:04:35 +0700

    kivy

--- mobile_app/Tutorial/tutorial_12_Navigation_Drawer.py
+++ mobile_app/Tutorial/tutorial_12_Navigation_Drawer.py
@@ -5,9 +5,55 @@ from kivy.core.window import Window
 Window.size = (300, 500)
 
 navigation_helper = """
-
-
-
+Screen:
+    NavigationLayout
+        ScreenManager:
+            Screen:
+            
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDToolbar:
+                        title: 'Demo Application'
+                        left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
+                        elevation:10
+                    Widget:
+                    
+        MDNavigationDrawer:
+            id: nav_drawer
+            
+            BoxLayout:
+                orientation: 'vertical'
+                
+                Image:
+                    source: 'chaiwat.jpg'
+                
+                MDLabel:
+                    text: ' Chaiwat'
+                    font_style: 'Subtitle1'
+                    size_hint_y: None
+                    height: self.texture_size[1]
+                    
+                MDLabel:
+                    text: ' chaiwat.klaew@gmail.com'
+                    font_style: 'Caption'
+                    size_hint_y: None
+                    height: self.texture_size[1]
+                
+                ScrollView:
+                    MDList:
+                        OneLineIconListItem:    
+                            text: 'Profile'
+                            IconLeftWidget:
+                                icon: 'face-profile'
+                        OneLineIconListItem:    
+                            text: 'Upload'
+                            IconLeftWidget:
+                                icon: 'file-upload'
+                        OneLineIconListItem:
+                            text: 'Logout'
+                            IconLeftWidget:
+                                icon: 'file-plus' 
+                
 """
 
 class DemoApp(MDApp):
