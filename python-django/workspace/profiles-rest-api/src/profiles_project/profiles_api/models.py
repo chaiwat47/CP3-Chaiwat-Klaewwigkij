ba85e85df1083e16fd669319d2cf2986a32c17a0
chaiwat47 <58399208+chaiwat47@users.noreply.github.com>
2020-11-09 14:34:13 +0700

    update

--- python-django/workspace/profiles-rest-api/src/profiles_project/profiles_api/models.py
+++ python-django/workspace/profiles-rest-api/src/profiles_project/profiles_api/models.py
@@ -1,11 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbtractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    '''Helps Django work with our custom user model.'''
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile object."""
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+
+
+
+
+
+
+
 
 
 # Create your models here.
-class UserProfile(AbstracBaseUser, PermissionsMixin):
-    '''Represent a "user profile" inside our system.'''
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represent a "user profile" inside our system."""
 
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -30,4 +67,5 @@ class UserProfile(AbstracBaseUser, PermissionsMixin):
 
     def __str__(self):
         """Django uses this when it needs to convert the object to a string"""
+
         return self.email
